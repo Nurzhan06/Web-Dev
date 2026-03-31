bf426a8055a3788a6654f15a6625533859bbb2a8
Nurzhan06 <n_abdik@kbtu.kz>
2026-04-01 00:35:12 +0500

    Add files via upload

---
+++ LAB9/shop-back/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import (
+    CategoryViewSet,
+    ProductViewSet
+)
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
+
+urlpatterns = router.urls
